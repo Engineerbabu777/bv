0232b21e5437e29100c1b2d24ce97543d3271f8c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-09 19:52:31 +0500

    add: size and options states in index home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+
 import { Inter } from "next/font/google";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +8,12 @@ export default function Home() {
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D>();
+
+  const [size, setSize] = useState({width:0,height:0});
+  const [options, setOptions] = useState<CtxOptions>({
+    lineColor:"#000",
+    lineWidth:5,
+  })
 
 
   return (
