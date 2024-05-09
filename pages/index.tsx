4386215fccf14ded63f3dd1cd05e8299f8695300
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-09 19:50:35 +0500

    Add: added canvasRef, ctxRef

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D>();
+
+
   return (
    <>
    
